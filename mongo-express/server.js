b8cd11b5962a5f9da37c5361440da28d749d48cc
droopypok <158001887+droopypok@users.noreply.github.com>
2024-03-29 22:44:28 +0800

    test2

--- mongo-express/server.js
+++ mongo-express/server.js
@@ -27,5 +27,5 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
 
-  ("i am ken and i am typing hello");
+  "i am ken and i am typing hello"("testing 2");
 });
