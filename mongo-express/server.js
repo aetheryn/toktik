b02ce8f65d9a7662662e65220828c139d293a91a
Gabrielle <gabriellenatashaa@gmail.com>
2024-03-29 21:46:53 +0800

    Set up starter codes

---
+++ mongo-express/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const connectDB = require("./src/db/db");
+
+connectDB();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}.`);
+});
