43286d74dd90348e591ec811abf0bea5aaaa4ced
droopypok <158001887+droopypok@users.noreply.github.com>
2024-04-01 11:02:45 +0800

    css not done

--- react-node/src/pages/LoginPage.jsx
+++ react-node/src/pages/LoginPage.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className={styles.container}>
+      <div className={styles.loginContainer}>
+        <div id="heading title">
+          <h1>TokTik</h1>
+        </div>
+        {/* username password 1 Login button 1  */}
+        <div id="buttons seperate login usrename and password">
+          <div id="username + password div">
+            <div id="username"></div>
+            <div id="password"></div>
+          </div>
+          <div id="login">
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
