43136ca0feb7f2a3b6de9232470f9a2157cf70b1
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-02 22:09:56 +0800

    update homepage to test display of videos

--- react-node/src/pages/HomePage.jsx
+++ react-node/src/pages/HomePage.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:5001/videos");
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        setVideos(data);
+      }
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        console.log(error.message);
+      }
+    }
+  };
+  useEffect(() => {
+    getVideos();
+    console.log(videos);
+  }, []);
+
   return (
     <div>
       <h1>Homepage</h1>
+      <ul>
+        {videos.map((video, index) => (
+          <li key={index}>
+            <h2>{video.title}</h2>
+            <p>{video.description}</p>
+            <img src={video.url} />
+            <video src={video.url} controls />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
