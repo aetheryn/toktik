3aa4abc6814c85c587565fdce4e35a2cae234d9a
Gabrielle <gabriellenatashaa@gmail.com>
2024-04-02 17:52:08 +0800

    Link user list & chats

--- react-node/src/pages/DirectMessage.jsx
+++ react-node/src/pages/DirectMessage.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import DMList from "../components/DMList";
 import Chat from "../components/Chat";
 import useFetch from "../hooks/useFetch";
+import UserinDM from "../components/UserinDM";
 
 const DirectMessage = () => {
   const fetchMessages = useFetch();
   const [allMessages, setAllMessages] = useState([]);
   const [usersInDMs, setUsersInDMs] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const loggedInUser = "Bryan";
   let senders;
@@ -34,8 +36,11 @@ const DirectMessage = () => {
 
       console.log(response);
       if (response.ok) {
-        setAllMessages(response.data);
-        getUsersInDMs(response.data);
+        const data = [...response.data];
+        data.reverse();
+
+        setAllMessages(data);
+        getUsersInDMs(data);
       }
     } catch (error) {
       if (error.name !== "AbortError") {
@@ -62,15 +67,57 @@ const DirectMessage = () => {
     setUsersInDMs(usersEngaged);
   };
 
+  const handleUserSelect = (user) => {
+    setSelectedUser(user);
+    setShowChat(true);
+  };
+
   return (
     <div className="row">
       <div className="col-1"></div>
-      <div className="col-3">
-        <DMList allMessages={allMessages} usersInDMs={usersInDMs}></DMList>
+      <div
+        className="col-3 centered"
+        style={{
+          backgroundColor: "black",
+          height: "640px",
+          width: "360px",
+          borderRadius: "30px",
+          display: "block",
+          paddingLeft: "-1.5rem",
+        }}
+      >
+        <h1
+          style={{
+            marginTop: "30px",
+            marginBottom: "30px",
+            fontSize: "1em",
+          }}
+        >
+          Direct Messages
+        </h1>
+
+        {usersInDMs.map((user) => {
+          return (
+            <UserinDM
+              user={user}
+              handleUserSelect={handleUserSelect}
+            ></UserinDM>
+          );
+        })}
       </div>
-      <div className="col">
-        <Chat allMessages={allMessages} usersInDMs={usersInDMs}></Chat>
+      <div
+        className="col centered"
+        style={{
+          backgroundColor: "black",
+          height: "640px",
+          borderRadius: "30px",
+          display: "block",
+          marginLeft: "10px",
+        }}
+      >
+        {showChat && <Chat selectedUser={selectedUser}></Chat>}
       </div>
+
       <div className="col-1"></div>
     </div>
   );
