505022b6fabe7238cb2850db9b15c90897044f1b
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-02 18:46:02 +0800

    update controller for aws s3

--- mongo-express/src/controllers/videos.js
+++ mongo-express/src/controllers/videos.js
@@ -1,5 +1,20 @@
 const { Videos } = require("../models/Videos");
 
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
+  region: bucketRegion,
+});
+
 const seedVideo = async (req, res) => {
   try {
     await Videos.deleteMany({});
@@ -130,6 +145,17 @@ const uploadVideo = async (req, res) => {
     console.log("req.body", req.body);
     console.log("req.file", req.file);
     req.file.buffer;
+
+    const params = {
+      Bucket: bucketName,
+      Key: req.file.originalname,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+    };
+
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+
     res.json({});
   } catch (error) {
     console.error(error.message);
