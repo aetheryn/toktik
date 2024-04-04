a8443682ae527ada918e7b2c721e3aee6b3b07ba
droopypok <158001887+droopypok@users.noreply.github.com>
2024-04-04 11:05:19 +0800

    comments for cm page

--- react-node/src/pages/ContentModerator.jsx
+++ react-node/src/pages/ContentModerator.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const ContentModerator = () => {
+  const fetchData = useFetch();
+
+  // fetchData for videoes that are flagged
+
+  // function for confirming video is OK
+
+  // function for confirming flagged video
+
   return <div></div>;
 };
 
