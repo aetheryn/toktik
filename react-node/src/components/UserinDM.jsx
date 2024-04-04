22b4e08848be663eb63234f295fab263e0d26fc4
Gabrielle <gabriellenatashaa@gmail.com>
2024-04-04 11:09:34 +0800

    Display last text message

--- react-node/src/components/UserinDM.jsx
+++ react-node/src/components/UserinDM.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Chat from "./Chat";
 
 const UserinDM = (props) => {
+  const [lastMessage, setLastMessage] = useState("");
+
+  const getLastMsg = (messages) => {
+    const lastMsg = messages.find(
+      (message) =>
+        message.receiver_id == props.user || message.sender_id == props.user
+    );
+    setLastMessage(lastMsg.content);
+  };
+
+  useEffect(() => {
+    getLastMsg(props.allMessages);
+  }, [props.allMessages]);
+
   return (
     <>
       <div
@@ -10,7 +24,8 @@ const UserinDM = (props) => {
           backgroundColor: "#c60060",
           height: "70px",
           alignItems: "center",
-          padding: "30px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
         }}
         onClick={() => {
           props.handleUserSelect(props.user);
@@ -22,13 +37,21 @@ const UserinDM = (props) => {
             borderRadius: "50px",
             height: "50px",
             width: "50px",
+            position: "absolute",
           }}
         ></div>
         <div
-          style={{ overflow: "hidden", textAlign: "left", paddingLeft: "20px" }}
+          style={{
+            overflow: "hidden",
+            textAlign: "left",
+            paddingLeft: "60px",
+            display: "grid",
+          }}
         >
           <div style={{ fontWeight: "700" }}>{props.user}</div>
-          <div style={{ fontSize: "x-small" }}>Last text message</div>
+          <div style={{ fontSize: "x-small", overflow: "hidden" }}>
+            {lastMessage}
+          </div>
         </div>
       </div>
     </>
