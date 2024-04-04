811c4f99661d082c827af643cb112aa72fb708c3
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-04 11:23:18 +0800

    update react upload

--- react-node/src/pages/Upload.jsx
+++ react-node/src/pages/Upload.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import "./Upload.css";
+import UserContext from "../context/user";
 
 const Upload = () => {
-  const [file, setFile] = useState("");
-  const [caption, setCaption] = useState("");
+  const userCtx = useContext(UserContext);
 
+  const [file, setFile] = useState("");
+  const [title, setTitle] = useState("");
+  const [preview, setPreview] = useState(false);
+
+  const inputRef = useRef();
+  // to track progress bar
+  const [progress, setProgress] = useState(0);
+  // to track upload status - "select", "uploading", "done"
+  const [uploadStatus, setUploadStatus] = useState("select");
+
+  // post to backend
   const submit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData();
-    formData.append("image", file);
-    formData.append("caption", caption);
+    formData.append("fileName", file);
+    formData.append("title", title);
+    formData.append("username", userCtx.username);
 
     try {
       const res = await fetch("http://127.0.0.1:6001/videos/videoupload", {
@@ -26,23 +39,60 @@ const Upload = () => {
     }
   };
 
+  // handle on change for file upload
+  const handleFileChange = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const onChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  useEffect(() => {
+    if (!file) {
+      setPreview(undefined);
+      return;
+    }
+    const fileUrl = URL.createObjectURL(file);
+    setPreview(fileUrl);
+    return () => URL.revokeObjectURL(fileUrl);
+  }, [file]);
+
   return (
     <>
-      <h1>Upload page</h1>
-      <div>
+      <div className="container">
         <form onSubmit={submit}>
           <input
-            onChange={(e) => setFile(e.target.files[0])}
+            ref={inputRef}
+            onChange={handleFileChange}
             type="file"
             accept="image/*,video/*"
+            style={{ display: "none" }}
           ></input>
-          <input
-            value={caption}
-            onChange={(e) => setCaption(e.target.value)}
-            type="text"
-            placeholder="Caption"
-          ></input>
-          <button type="submit">Submit</button>
+          {!file && (
+            <button className="file-btn" onClick={onChooseFile}>
+              Upload File
+            </button>
+          )}
+
+          {file && (
+            <>
+              <h1>Video Preview</h1>
+              <video src={preview} style={{ width: 200, height: 200 }} />
+
+              <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                type="text"
+                placeholder="Title"
+                style={{ color: "black" }}
+              ></input>
+            </>
+          )}
+
+          <button type="submit">Post</button>
         </form>
       </div>
     </>
