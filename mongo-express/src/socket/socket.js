451c1a0630fb017120bc4153fdf3610b200fafe6
Gabrielle <gabriellenatashaa@gmail.com>
2024-04-05 15:13:08 +0800

    Change routing

--- mongo-express/src/socket/socket.js
+++ mongo-express/src/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT"],
   },
 });
 
