3e382d05b043ce766c10c6ab9f4aae1e2936ec85
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-09 01:16:01 +0800

    update video component for reporting

--- react-node/src/components/Video.jsx
+++ react-node/src/components/Video.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../components/Video.css";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import CommentIcon from "@mui/icons-material/Comment";
+import FlagIcon from "@mui/icons-material/Flag";
 import ShareIcon from "@mui/icons-material/Share";
+import useFetch from "../hooks/useFetch";
 
 const Video = (props) => {
+  const fetchData = useFetch();
+  // state to track color
+  const [color, setColor] = useState("primary");
+  // state to track reported status
+  const [reported, setReported] = useState(props.video.reported);
+
+  // function for report button
+  const reportVideo = async (flaggedId) => {
+    const res = await fetchData(
+      "/videos/flagged/" + flaggedId,
+      "PATCH",
+      {
+        reported: !reported,
+      },
+      undefined
+    );
+    if (res.ok) {
+      console.log("clicked, video reported smh");
+      setReported(reported);
+    }
+
+    props.handleReportChange(flaggedId, !reported);
+    // to change color
+    setColor((prevColor) =>
+      prevColor === "primary" ? "secondary" : "primary"
+    );
+  };
+
   return (
     <>
       <div className="video-display">
@@ -19,6 +49,7 @@ const Video = (props) => {
             right: 15,
             top: 500,
             fontSize: 28,
+            zIndex: 1600,
           }}
         ></FavoriteIcon>
         <div className="likes">{props.video.likes.length}</div>
@@ -32,11 +63,24 @@ const Video = (props) => {
           }}
         ></CommentIcon>
         <div className="comments">{props.video.comments.length}</div>
-        <ShareIcon
+
+        <FlagIcon
+          onClick={() => reportVideo(props.video._id)}
           style={{
             position: "absolute",
             right: 15,
             top: 620,
+            fontSize: 28,
+            zIndex: 1600,
+            color: color,
+          }}
+        ></FlagIcon>
+
+        <ShareIcon
+          style={{
+            position: "absolute",
+            right: 15,
+            top: 670,
             fontSize: 28,
           }}
         ></ShareIcon>
