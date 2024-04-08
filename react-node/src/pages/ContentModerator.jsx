538ceb91d989a8e460879e2aafaf4f697c65f0d1
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-08 17:33:16 +0800

    update content moderator functions

--- react-node/src/pages/ContentModerator.jsx
+++ react-node/src/pages/ContentModerator.jsx
@@ -6,6 +6,7 @@ import "./ContentModerator.css";
 const ContentModerator = () => {
   const fetchData = useFetch();
   const [flaggedVideos, setFlaggedVideos] = useState([]);
+  const [videoStatus, setVideoStatus] = useState(false);
 
   // fetchData for videoes that are flagged
   const getFlaggedVideos = async () => {
@@ -16,27 +17,56 @@ const ContentModerator = () => {
       undefined
     );
     if (res.ok) {
+      console.log(res.data);
       setFlaggedVideos(res.data);
       console.log(flaggedVideos);
     }
   };
   useEffect(() => {
     getFlaggedVideos();
-  }, []);
+  }, [videoStatus]);
 
   // function for confirming video is OK
+  const videoPass = async () => {
+    let flaggedId = flaggedVideos[0]._id;
+    console.log(flaggedId);
+    const res = await fetchData(
+      "/videos/flagged/" + flaggedId,
+      "PATCH",
+      {
+        reported: false,
+      },
+      undefined
+    );
+    if (res.ok) {
+      setVideoStatus(true);
+      console.log(videoStatus);
+    }
+  };
 
   // function for confirming flagged video
+  const videoFail = async () => {
+    let flaggedId = flaggedVideos[0]._id;
+    const res = await fetchData(
+      "/videos/" + flaggedId,
+      "DELETE",
+      undefined,
+      undefined
+    );
+    if (res.ok) {
+      setVideoStatus(true);
+    }
+  };
 
   return (
     <>
-      <button>YES</button>
+      <button onClick={videoPass}>YES</button>
       <div className="flagged-videos">
         {flaggedVideos.map((video, index) => (
           <Video key={index} video={video} id={index}></Video>
         ))}
       </div>
-      <button>NO</button>
+      <button onClick={videoFail}>NO</button>
     </>
   );
 };
