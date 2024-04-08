3ecbf5dc4bbf6486d47f9066e7fc0dd30e4edc89
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-09 01:15:44 +0800

    update home page for reporting

--- react-node/src/pages/HomePage.jsx
+++ react-node/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Profile from "./Profile";
 import Video from "../components/Video";
 import "./HomePage.css";
 
 const HomePage = () => {
   const [videos, setVideos] = useState([]);
-  const navigate = useNavigate();
+  // state for reported videos
+  const [reported, setReported] = useState(false);
 
   const getVideos = async () => {
     try {
@@ -14,7 +15,11 @@ const HomePage = () => {
       if (res.ok) {
         const data = await res.json();
         console.log(data);
-        setVideos(data);
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].reported === false) {
+            setVideos(data);
+          }
+        }
       }
     } catch (error) {
       if (error.name !== "AbortError") {
@@ -27,11 +32,22 @@ const HomePage = () => {
     console.log(videos);
   }, []);
 
+  // to remove the flagged video from the homepage
+  // creates a new array via filter -> checks the whether the videoid sent in exists in the current array
+  const handleReportChange = (videoId) => {
+    setVideos(videos.filter((video) => video._id !== videoId));
+  };
+
   return (
     <div>
       <div className="homepage">
         {videos.map((video, index) => (
-          <Video key={index} video={video} id={index} />
+          <Video
+            key={index}
+            video={video}
+            id={index}
+            handleReportChange={handleReportChange}
+          />
         ))}
       </div>
     </div>
