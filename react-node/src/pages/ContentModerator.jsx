9e0d0a797ad1bf38b9ab2928c167d66675022b56
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-08 16:23:56 +0800

    update content moderator page

--- react-node/src/pages/ContentModerator.jsx
+++ react-node/src/pages/ContentModerator.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
+import Video from "../components/Video";
+import "./ContentModerator.css";
 
 const ContentModerator = () => {
   const fetchData = useFetch();
+  const [flaggedVideos, setFlaggedVideos] = useState([]);
 
   // fetchData for videoes that are flagged
+  const getFlaggedVideos = async () => {
+    const res = await fetchData(
+      "/videos/flagged/",
+      "GET",
+      undefined,
+      undefined
+    );
+    if (res.ok) {
+      setFlaggedVideos(res.data);
+      console.log(flaggedVideos);
+    }
+  };
+  useEffect(() => {
+    getFlaggedVideos();
+  }, []);
 
   // function for confirming video is OK
 
   // function for confirming flagged video
 
-  return <div></div>;
+  return (
+    <>
+      <button>YES</button>
+      <div className="flagged-videos">
+        {flaggedVideos.map((video, index) => (
+          <Video key={index} video={video} id={index}></Video>
+        ))}
+      </div>
+      <button>NO</button>
+    </>
+  );
 };
 
 export default ContentModerator;
